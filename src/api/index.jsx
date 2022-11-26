d7c8964ccebcc9c39f9436566a3f3c216c1a6813
ProgrammerFront077 <alirezakh0090@gmail.com>
2022-11-26 17:38:09 +0330

    Initial commit

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const ajaxApiFitness = axios.create({
   baseURL: 'https://exercisedb.p.rapidapi.com',
   headers: {
-    'X-RapidAPI-Key': 'f060159861msh0aded4fae927f21p178aa6jsnd2f0fabb4c54',
+    'X-RapidAPI-Key': '809584107fmsh4e7c58c7ff7046cp14ec92jsn7ae0da7b8d03',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
   },
 });
